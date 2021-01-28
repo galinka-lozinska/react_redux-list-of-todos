83d310c0c4ccbedeb39fbed86737face916ba150
Halynka Lozinska <galinkalozinska96@gmail.com>
2021-01-28 23:45:52 +0200

    Updates

---
+++ precache-manifest.544f2ad58d37b04dc4e99015c3575ba6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "213854f4d35a9eab21d906e54352f5fc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6187ef920ac7bfad15cd",
+    "url": "./static/css/main.378b3899.chunk.css"
+  },
+  {
+    "revision": "d60b801ef74ea4e39b48",
+    "url": "./static/js/2.a971278a.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "./static/js/2.a971278a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6187ef920ac7bfad15cd",
+    "url": "./static/js/main.26a1c1ff.chunk.js"
+  },
+  {
+    "revision": "6e2c239f7a333241bc85",
+    "url": "./static/js/runtime-main.cb0ead06.js"
+  }
+]);
